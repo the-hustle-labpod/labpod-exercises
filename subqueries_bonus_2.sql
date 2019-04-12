d4e94e213e1762bf2b976b17a7af3d764eeb221a
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-12 12:14:14 -0500

    added note to subqueries bonus 2

--- subqueries_bonus_2.sql
+++ subqueries_bonus_2.sql
@@ -1,3 +1,5 @@
+-- NOTE: This code will not currently run, as is.
+-- The Employees database needs to be downloaded and installed first.
 SELECT e.first_name, e.last_name
 FROM employees AS e
        JOIN salaries s on e.emp_no = s.emp_no
