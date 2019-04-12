d650c883760a68db61f22c1eebb28862619d25ce
Dwight Bemisderfer <dwight.bemisderfer@gmail.com>
2019-04-12 13:14:21 -0500

    refactored code to use ONLY subqueries and NOT join clauses

--- subqueries_bonus_1.sql
+++ subqueries_bonus_1.sql
@@ -1,5 +1,20 @@
 -- NOTE: This code will not currently run, as is.
 -- The Employees database needs to be downloaded and installed first.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+  SELECT dept_no
+  FROM dept_manager
+  WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE gender = 'F'
+      AND emp_no IN (
+      SELECT emp_no
+      FROM dept_manager
+      WHERE to_date > CURDATE())));
+
+-- Alternate solution
 SELECT d.dept_name
 FROM employees as e
        JOIN dept_manager dm on e.emp_no = dm.emp_no
--- subqueries_bonus_2.sql
+++ subqueries_bonus_2.sql
@@ -1,5 +1,18 @@
 -- NOTE: This code will not currently run, as is.
 -- The Employees database needs to be downloaded and installed first.
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+  SELECT emp_no
+  FROM salaries
+  WHERE salary IN (
+    (SELECT MAX(salary)
+     FROM salaries)
+  )
+);
+
+
+-- Alternate solution
 SELECT e.first_name, e.last_name
 FROM employees AS e
        JOIN salaries s on e.emp_no = s.emp_no
